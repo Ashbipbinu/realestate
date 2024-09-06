8ee6cf79b716f95ce1b667caa25b594c2e54ebec
Ashbipbinu <abinu@thoughtlinetech.com>
2024-09-06 20:50:45 +0530

    components created and commiting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+
 
 function App() {
  
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Router>
+        <Route path='/' element={<Home/>}/>
+        <Route path= '/profile' element={<Profile/>}/>
+        <Route path= '/about' element={<About/>}/>
+        <Route path= '/signin' element={<Signin/>}/>
+        <Route path= '/signup' element={<Signup/>}/>
+      </Router>
+    </BrowserRouter>
   )
 }
 
