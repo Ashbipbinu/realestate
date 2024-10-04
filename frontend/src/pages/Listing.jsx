6ecf817e343730018ea1b2b43440c8856945106f
Ashbipbinu <abinu@thoughtlinetech.com>
2024-10-04 13:51:52 +0530

    minor change in listing

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -147,7 +147,7 @@ const Listing = () => {
               </li>
             </ul>
             {/* && listing?.userRef !== currentUser._id && !contact */}
-            {currentUser  && !contact &&(
+            {currentUser && listing?.userRef !== currentUser._id && !contact  && !contact &&(
               <button
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
